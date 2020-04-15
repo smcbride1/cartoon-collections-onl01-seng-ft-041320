4704f8cd43783d764dcd6aa6c30df200934040a8
Stephen McBride <stevemcbride3@gmail.com>
2020-04-15 20:14:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,10 +2,15 @@ def roll_call_dwarves(names)# code an argument here
   # Your code here
   names.each_with_index do | name, index |
     puts "#{index} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls)# code an argument here
   # Your code here
+  calls_modified = []
+  calls.each do | call |
+    calls_modified.push("#{call.capitalize}!")
+  end
 end
 
 def long_planeteer_calls# code an argument here
