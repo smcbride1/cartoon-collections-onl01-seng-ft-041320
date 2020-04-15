9a6e0c1ebdf3ef5c00fca6c6ba9dde32fc55a6d5
Stephen McBride <stevemcbride3@gmail.com>
2020-04-15 20:31:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,8 @@ def long_planeteer_calls(calls)# code an argument here
   calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(strings)# code an argument here
   # the array below is here to help
+  strings.find {|string| cheese_types.include(string)}
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
