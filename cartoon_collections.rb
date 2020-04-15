58796d0582459b9a7557a2e6f2488ebbe21e77d1
Stephen McBride <stevemcbride3@gmail.com>
2020-04-15 20:27:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,9 @@ def summon_captain_planet(calls)# code an argument here
   calls.map! {|call| "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.any? {|call| call.length > 4}
 end
 
 def find_the_cheese# code an argument here
