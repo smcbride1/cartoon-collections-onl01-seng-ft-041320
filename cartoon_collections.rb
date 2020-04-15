c537a96e8075e94fdd8747fcd36c15cd8ab1aa7c
Stephen McBride <stevemcbride3@gmail.com>
2020-04-15 20:18:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,10 +7,7 @@ end
 
 def summon_captain_planet(calls)# code an argument here
   # Your code here
-  calls_modified = []
-  calls.each do | call |
-    calls_modified.push("#{call.capitalize}!")
-  end
+  calls.map! {|call| "#{call.capitalize}!"}
 end
 
 def long_planeteer_calls# code an argument here
